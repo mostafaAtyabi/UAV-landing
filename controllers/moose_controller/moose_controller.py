79d6bce172585a46aacd7488d0a24a1a499c4c34
mostafa <atyabimostafa00@gmail.com>
2024-08-30 15:49:19 +0330

    add controller for moose

--- controllers/moose_controller/moose_controller.py
+++ controllers/moose_controller/moose_controller.py
@@ -1,32 +1,58 @@
-"""moose_controller controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
-
-# create the Robot instance.
+from controller import Robot, Motor
+from time import sleep
+# Initialize the Webots Robot instance
 robot = Robot()
 
-# get the time step of the current world.
+# Get the time step of the current world
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+# Motor names based on the C code
+motor_names = [
+    "left motor 1", "left motor 2", "left motor 3", "left motor 4",
+    "right motor 1", "right motor 2", "right motor 3", "right motor 4"
+]
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+# Initialize the motors
+motors = [robot.getDevice(name) for name in motor_names]
+
+# Set the motors' position to infinity for continuous rotation
+for motor in motors:
+    motor.setPosition(float('inf'))
+
+# Function to set the motor velocities for both sides
+def set_velocity(left_speed, right_speed):
+    for i in range(4):
+        motors[i].setVelocity(left_speed)  # Left motors
+    for i in range(4, 8):
+        motors[i].setVelocity(right_speed)  # Right motors
+
+# Define the speeds
+forward_speed = 1.5  # Adjust the speed as necessary
+backward_speed = -1.5  # Negative for backward movement
+move_duration = 20000  # Adjust this for how long to move in each direction
+
+# Main loop
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
 
-    # Process sensor data here.
+    # Move forward
+    for _ in range(move_duration):
+        set_velocity(forward_speed, forward_speed)
+        # sleep(1)
+        if robot.step(timestep) == -1:
+            break
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    # Stop briefly before changing direction
+    set_velocity(0.0, 0.0)
 
-# Enter here exit cleanup code.
+
+
+    # Move backward
+    for _ in range(move_duration):
+        set_velocity(backward_speed, backward_speed)
+        # sleep(1)
+        if robot.step(timestep) == -1:
+            break
+
+    # Stop briefly before changing direction
+    set_velocity(0.0, 0.0)
+
